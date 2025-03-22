9cbd0bf06c9cbc7334aad3d02f8b584b94ee1981
kesav-v <kesav@viswanadha.net>
2025-03-22 12:56:07 -0700

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Kesav V's Website</h1>
+        <p>This site is currently under construction.</p>
+        <p>Check back soon for more content!</p>
       </header>
     </div>
   );
